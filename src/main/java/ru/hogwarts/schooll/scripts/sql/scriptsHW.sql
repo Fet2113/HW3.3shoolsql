09f0828593852a241f4eddb2d854a710bde9b182
Fet2113 <super.gjoll@yandex.ru>
2023-11-23 20:25:01 +0300

    HW TechnicalInterviewTASK15

--- src/main/java/ru/hogwarts/schooll/scripts/sql/scriptsHW.sql
+++ src/main/java/ru/hogwarts/schooll/scripts/sql/scriptsHW.sql
@@ -1,6 +1,7 @@
 // Таблица employee с колонками name, department, salary.
-// Вывести максимальную зарплату (salary) по каждому отделу, имеющему больше одного сотрудника.
-//и среднюю зарплату (salary) по каждому отделу, имеющему больше одного сотрудника.
+// Вывести максимальную зарплату (salary) и среднюю зарплату (salary)
+// по каждому отделу, имеющему больше одного сотрудника.
 select avg(salary), max(salary) from employee
-group by department having count(*) >1 ;
+group by department having count(*) > 1;
+
 
